b29dc7a54f73bd4be7da27418971a6f696436696
Bibin Sebastian <bisebastian@DELC02QP51SG8WN.sea.corp.expecn.com>
2018-06-15 09:55:33 +0530

    code cleanup

--- kafka/src/main/java/com/expedia/adaptivealerting/kafka/router/MetricRouter.java
+++ kafka/src/main/java/com/expedia/adaptivealerting/kafka/router/MetricRouter.java
@@ -16,12 +16,52 @@
 package com.expedia.adaptivealerting.kafka.router;
 
 import com.expedia.adaptivealerting.kafka.util.AppUtil;
+import com.expedia.adaptivealerting.kafka.util.BaseStreamRunnerBuilder;
+import com.expedia.www.haystack.commons.entities.MetricPoint;
+import com.expedia.www.haystack.commons.kstreams.app.StreamsRunner;
 import com.typesafe.config.Config;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MetricRouter {
 
     public static void main(String[] args) {
         Config appConfig = AppUtil.getAppConfig("metricrouter");
-        AppUtil.launchStreamRunner(new MetricRouterStreamBuilder().build(appConfig));
+        AppUtil.launchStreamRunner(new StreamRunnerBuilder().build(appConfig));
+    }
+
+    public static class StreamRunnerBuilder extends BaseStreamRunnerBuilder {
+        @Override
+        public StreamsRunner build(Config appConfig) {
+            final StreamsBuilder builder = createStreamsBuilder(appConfig);
+
+            return createStreamsRunner(appConfig, builder);
+        }
+
+        private static StreamsBuilder createStreamsBuilder(Config appConfig) {
+            final StreamsBuilder builder = new StreamsBuilder();
+            final KStream<String, MetricPoint> metrics = builder.stream(appConfig.getString("topic"));
+
+            metrics.filter(StreamRunnerBuilder::isConstant).to("constant-metrics");
+            metrics.filter(StreamRunnerBuilder::isEwma).to("ewma-metrics");
+            metrics.filter(StreamRunnerBuilder::isPewma).to("pewma-metrics");
+            return builder;
+        }
+
+        // TODO: add real routing conditions
+        private static boolean isConstant(String key, MetricPoint metricPoint) {
+            return Arrays.asList("latency", "duration").contains(metricPoint.metric());
+        }
+
+        private static boolean isEwma(String key, MetricPoint metricPoint) {
+            return Collections.singletonList("ewma").contains(metricPoint.metric());
+        }
+
+        private static boolean isPewma(String key, MetricPoint metricPoint) {
+            return Collections.singletonList("pewma").contains(metricPoint.metric());
+        }
     }
 }
---
+++ kafka/src/main/java/com/expedia/adaptivealerting/kafka/util/BaseStreamRunnerBuilder.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 Expedia Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expedia.adaptivealerting.kafka.util;
+
+import com.expedia.www.haystack.commons.health.HealthStatusController;
+import com.expedia.www.haystack.commons.health.UpdateHealthStatusFile;
+import com.expedia.www.haystack.commons.kstreams.app.StateChangeListener;
+import com.expedia.www.haystack.commons.kstreams.app.StreamsFactory;
+import com.expedia.www.haystack.commons.kstreams.app.StreamsRunner;
+import com.typesafe.config.Config;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
+public abstract class BaseStreamRunnerBuilder {
+    public abstract StreamsRunner build(Config config);
+
+    protected StreamsRunner createStreamsRunner(Config appConfig, StreamsBuilder builder) {
+        String healthStatusPath = appConfig.getString("health.status.path");
+        String topic = appConfig.getString("topic");
+
+        HealthStatusController healthStatusController = new HealthStatusController();
+        healthStatusController.addListener(new UpdateHealthStatusFile(healthStatusPath));
+
+        StateChangeListener stateChangeListener = new StateChangeListener(healthStatusController);
+
+        StreamsConfig streamsConfig = new StreamsConfig(configToProp(appConfig.getConfig("streams")));
+
+        StreamsFactory streamsFactory = new StreamsFactory(builder::build, streamsConfig, topic);
+
+        return new StreamsRunner(streamsFactory, stateChangeListener);
+    }
+
+    private Properties configToProp(Config config) {
+        Properties props = new Properties();
+        config.entrySet().forEach((entry) -> {
+            props.setProperty(entry.getKey(), entry.getValue().unwrapped().toString());
+        });
+        return props;
+    }
+}
