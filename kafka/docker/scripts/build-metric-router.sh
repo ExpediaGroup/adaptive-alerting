8d5a8f4d5e10ab7585c1988464564af68e28c3e9
shsethi <shsethi@expedia.com>
2018-06-19 16:06:29 +0530

    added scripts to build docker images and publish it using Travis

--- kafka/docker/scripts/build-metric-router.sh
+++ kafka/docker/scripts/build-metric-router.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-APP_NAME="metric-router"
+APP_NAME="haystack-adaptive-alerting-metric-router"
 DOTTED_APP_NAME="metric.router"
 MAIN_CLASS="com.expedia.adaptivealerting.kafka.router.MetricRouter"
 
