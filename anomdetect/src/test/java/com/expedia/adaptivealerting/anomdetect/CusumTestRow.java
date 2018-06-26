8910e99f5e2753b4110c5540ad906f8a4cb5aab3
Karan Shah <kashah@expedia.com>
2018-06-26 15:12:22 +0530

    fixed formatting

--- anomdetect/src/test/java/com/expedia/adaptivealerting/anomdetect/CusumTestRow.java
+++ anomdetect/src/test/java/com/expedia/adaptivealerting/anomdetect/CusumTestRow.java
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * 
- */
 package com.expedia.adaptivealerting.anomdetect;
 
 import com.opencsv.bean.CsvBindByName;
