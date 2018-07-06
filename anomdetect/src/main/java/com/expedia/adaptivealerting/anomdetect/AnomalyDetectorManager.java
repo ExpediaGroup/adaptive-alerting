ed3eed86476a200a847d842bf9909e8ff1c1d930
Karan Shah <kashah@expedia.com>
2018-07-06 14:11:07 +0530

    resolved conflicts

--- anomdetect/src/main/java/com/expedia/adaptivealerting/anomdetect/AnomalyDetectorManager.java
+++ anomdetect/src/main/java/com/expedia/adaptivealerting/anomdetect/AnomalyDetectorManager.java
@@ -16,6 +16,10 @@
 package com.expedia.adaptivealerting.anomdetect;
 
 import com.expedia.adaptivealerting.core.data.MappedMpoint;
+import com.expedia.adaptivealerting.core.util.ReflectionUtil;
+import com.typesafe.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +34,7 @@ import static com.expedia.adaptivealerting.core.util.AssertUtil.notNull;
  * @author Willie Wheeler
  */
 public final class AnomalyDetectorManager {
+    private static final Logger log = LoggerFactory.getLogger(AnomalyDetectorManager.class);
     
     /**
      * Factories that know how to produce anomaly detectors on demand.
@@ -40,6 +45,17 @@ public final class AnomalyDetectorManager {
      * The managed detectors.
      */
     private final Map<UUID, AnomalyDetector> detectors = new HashMap<>();
+    
+    public AnomalyDetectorManager(Config factoryConfig) {
+        notNull(factoryConfig, "factoryConfig can't be null");
+        this.detectorFactories = new HashMap<>();
+        factoryConfig.entrySet().forEach(entry -> {
+            final String className = entry.getValue().unwrapped().toString();
+            final AnomalyDetectorFactory factory = (AnomalyDetectorFactory) ReflectionUtil.newInstance(className);
+            factory.init(factoryConfig);
+            detectorFactories.put(entry.getKey(), factory);
+        });
+    }
     
     /**
      * Creates a new anomaly detector manager.
@@ -52,10 +68,12 @@ public final class AnomalyDetectorManager {
     }
     
     /**
-     * Gets the anomaly detector for the given metric point, creating it if absent.
+     * Gets the anomaly detector for the given metric point, creating it if absent. Returns {@code null} if there's no
+     * {@link AnomalyDetectorFactory} defined for the mapped mpoint's detector type.
      *
      * @param mappedMpoint Mapped metric point.
-     * @return Anomaly detector for the given metric point.
+     * @return Anomaly detector for the given metric point, or {@code null} if there's no registered detector factory
+     * for the mapped mpoint's detector type.
      */
     public AnomalyDetector detectorFor(MappedMpoint mappedMpoint) {
         notNull(mappedMpoint, "mappedMpoint can't be null");
@@ -64,22 +82,31 @@ public final class AnomalyDetectorManager {
         if (detector == null) {
             final String detectorType = mappedMpoint.getDetectorType();
             final AnomalyDetectorFactory factory = detectorFactories.get(detectorType);
-            detector = factory.create(detectorUuid);
-            detectors.put(detectorUuid, detector);
+            if (factory == null) {
+                log.warn("No AnomalyDetectorFactory registered for detectorType={}", detectorType);
+            } else {
+                detector = factory.create(detectorUuid);
+                detectors.put(detectorUuid, detector);
+            }
         }
         return detector;
     }
     
     /**
+     * <p>
      * Convenience method to classify the mapped metric point, performing detector lookup behind the scenes. Note that
      * this method has a side-effect in that it updates the passed mapped metric point itself.
+     * </p>
+     * <p>
+     * Returns {@code null} if there's no detector defined for the given mapped mpoint.
+     * </p>
      *
      * @param mappedMpoint Mapped metric point.
-     * @return The mapped metric point.
+     * @return The mapped metric point, or {@code null} if there's no associated detector.
      */
     public MappedMpoint classify(MappedMpoint mappedMpoint) {
         notNull(mappedMpoint, "mappedMpoint can't be null");
-        AnomalyDetector detector = detectorFor(mappedMpoint);
+        final AnomalyDetector detector = detectorFor(mappedMpoint);
         MonitoredDetector monitorDetector = new MonitoredDetector(detector);
         monitorDetector.classify(mappedMpoint);
         return detectorFor(mappedMpoint).classify(mappedMpoint);
