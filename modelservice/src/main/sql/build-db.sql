efde60a30b4f791bf9f1d3cd8e1c02a193df3e1e
shsethi <shsethi@expedia.com>
2018-07-30 17:11:04 +0530

    schema for model service

--- modelservice/src/main/sql/build-db.sql
+++ modelservice/src/main/sql/build-db.sql
@@ -1,74 +1,83 @@
+# -------------
 DROP DATABASE IF EXISTS model_service;
 
-CREATE DATABASE model_service;
+create database model_service;
 
-USE model_service;
+use model_service;
 
 
-CREATE TABLE metric
+create table metric
 (
-  metric_id   INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  metric_uuid CHAR(32) UNIQUE          NOT NULL
+  id         int unsigned primary key not null auto_increment,
+  metric_key char(32) unique          not null
 );
 
-CREATE TABLE models
+create table model
 (
-  model_id      MEDIUMINT UNSIGNED PRIMARY KEY      NOT NULL AUTO_INCREMENT,
-  model_uuid    CHAR(32) UNIQUE                     NOT NULL,
-  hyperparams   JSON,
-  thresholds    JSON,
-  to_rebuild    BOOLEAN                                      DEFAULT FALSE,
-  last_build_ts TIMESTAMP
+  id                mediumint unsigned primary key not null auto_increment,
+  model_uuid        char(32) unique                not null,
+  hyperparams       json,
+  thresholds        json,
+  to_rebuild        boolean                                 default false,
+  training_location varchar(100),
+  last_build_ts     timestamp
 );
 
 
-CREATE TABLE metric_model_map
+create table metric_model
 (
-  metric_id INT UNSIGNED       NOT NULL,
-  model_id  MEDIUMINT UNSIGNED NOT NULL
+  metric_id int unsigned       not null,
+  model_id  mediumint unsigned not null,
+  unique index (metric_id, model_id),
+
+  constraint metric_id_fk foreign key (metric_id) references metric (id)
+    on delete cascade
+    on update cascade,
+  constraint model_id_fk foreign key (model_id) references model (id)
+    on delete cascade
+    on update cascade
 );
 
 -- =====================================================================================================================
--- PROCEDURES
+-- procedures
 -- =====================================================================================================================
 
-DROP PROCEDURE IF EXISTS add_new_model;
 
-CREATE PROCEDURE add_new_model(
-  metricUuid CHAR(32),
-  hyperParams JSON,
-  modelUuid CHAR(32)
+delimiter $$
+create procedure add_new_model(
+  _metric_key  char(32),
+  _hyperparams json,
+  _model_uuid  char(32))
+  begin
+    declare metricid int;
+    insert ignore into metric (metric_key) values (_metric_key);
+    select id
+    into metricid
+    from metric
+    where metric_key = _metric_key;
+    insert into model (model_uuid, hyperparams) values (_model_uuid, _hyperparams);
+    insert into metric_model (metric_id, model_id) values (metricid, last_insert_id());
+  end $$
+
+create procedure update_threshold(
+  _metric_key char(32),
+  _threshold  json,
+  _modeluuid  char(32)
 )
-  BEGIN
+  begin
+    declare modelid mediumint unsigned;
+    select model.id
+    into modelid
+    from metric_model
+      join model on metric_model.model_id = model.id
+    where metric_model.metric_id = (select id
+                                    from metric
+                                    where metric_key = _metric_key) and
+          model_uuid = _modeluuid;
 
-    DECLARE metricID INT;
+    update model
+    set thresholds = _threshold
+    where model.id = modelid;
 
-    INSERT IGNORE INTO metric (metric_uuid) VALUES (metricUuid);
-    SELECT metric_id INTO metricID FROM metric WHERE metric_uuid=metricUuid;
-
-    INSERT INTO  models (model_uuid, hyperparams) VALUES (modelUuid, hyperParams);
-    INSERT INTO metric_model_map (metric_id,model_id) VALUES (metricID, LAST_INSERT_ID());
-
-  END;
-
-DROP PROCEDURE IF EXISTS update_threshold;
-CREATE PROCEDURE update_threshold(
-  metricUuid CHAR(32),
-  thresHold JSON,
-  modelUuid CHAR(32)
-)
-  BEGIN
-    DECLARE modelID MEDIUMINT UNSIGNED;
-    SELECT models.model_id INTO modelID
-    FROM metric_model_map
-      JOIN models ON metric_model_map.model_id = models.model_id
-    WHERE metric_model_map.metric_id = (SELECT metric_id
-                                        FROM metric
-                                        WHERE metric_uuid = metricUuid) AND
-          model_uuid = modelUuid;
-
-    UPDATE models SET thresholds=thresHold WHERE model_id=modelID;
-
-  END;
-
-
+  end $$
+delimiter ;
