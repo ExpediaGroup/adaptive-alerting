d7b5bdf6f9feaa4f6abc7eb6843663bdfbe1f78a
Willie Wheeler <willie.wheeler@gmail.com>
2018-09-18 15:07:52 +1000

    Cleaned up the model service database config

--- modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/ModelServiceApp.java
+++ modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/ModelServiceApp.java
@@ -15,29 +15,13 @@
  */
 package com.expedia.adaptivealerting.modelservice;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ModelServiceApp {
 
-    @Autowired
-    private DatabaseSettings settings;
-
     public static void main(String[] args) {
         SpringApplication.run(ModelServiceApp.class, args);
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DataSource dataSource = new DataSource();
-        dataSource.setDriverClassName(settings.getDrivername());
-        dataSource.setUrl(settings.getUrl());
-        dataSource.setUsername(settings.getUsername());
-        dataSource.setPassword(settings.getPassword());
-        return dataSource;
     }
 }
