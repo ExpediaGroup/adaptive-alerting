c660740fd20f08d2009cd4c7c8c1ee2d9bd1a135
Willie Wheeler <willie.wheeler@gmail.com>
2018-09-25 23:11:09 +1000

    Updated deserializer to handle MetricData

--- kafka/src/main/java/com/expedia/adaptivealerting/kafka/serde/JsonPojoDeserializer.java
+++ kafka/src/main/java/com/expedia/adaptivealerting/kafka/serde/JsonPojoDeserializer.java
@@ -21,25 +21,37 @@
  */
 package com.expedia.adaptivealerting.kafka.serde;
 
+import com.expedia.metrics.MetricData;
+import com.expedia.metrics.MetricDefinition;
+import com.expedia.metrics.TagCollection;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 
 import java.util.Map;
+import java.util.Set;
 
 public class JsonPojoDeserializer<T> implements Deserializer<T> {
     private ObjectMapper objectMapper = new ObjectMapper();
-
     private Class<T> tClass;
-
+    
     /**
      * Default constructor needed by Kafka
      */
     public JsonPojoDeserializer() {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        
+        // FIXME This stuff doesn't belong here, as it violates the Open/Closed Principle.
+        // Chat with Peter about how we might most effectively incorporate the Jackson serde he created. [WLW]
+        this.objectMapper.addMixIn(MetricData.class, MetricDataMixin.class);
+        this.objectMapper.addMixIn(MetricDefinition.class, MetricDefinitionMixin.class);
+        this.objectMapper.addMixIn(TagCollection.class, TagCollectionMixin.class);
     }
-
+    
     @SuppressWarnings("unchecked")
     @Override
     public void configure(Map<String, ?> props, boolean isKey) {
@@ -54,24 +66,53 @@ public class JsonPojoDeserializer<T> implements Deserializer<T> {
             }
         }
     }
-
+    
     @Override
     public T deserialize(String topic, byte[] bytes) {
         if (bytes == null)
             return null;
-
+        
         T data;
         try {
             data = objectMapper.readValue(bytes, tClass);
         } catch (Exception e) {
             throw new SerializationException(e);
         }
-
+        
         return data;
     }
-
+    
     @Override
     public void close() {
-
     }
+    
+    private static class TagCollectionMixin {
+        
+        @JsonCreator
+        TagCollectionMixin(@JsonProperty("kv") Map<String, String> kv, @JsonProperty("v") Set<String> v) {
+        }
+        
+        @JsonIgnore
+        boolean isEmpty() {
+            return true;
+        }
+    }
+    
+    private static class MetricDefinitionMixin {
+        
+        @JsonCreator
+        MetricDefinitionMixin(@JsonProperty("tags") TagCollection tags, @JsonProperty("meta") TagCollection meta) {
+        }
+    }
+    
+    private static class MetricDataMixin {
+        
+        @JsonCreator
+        MetricDataMixin(
+                @JsonProperty("metricDefinition") MetricDefinition metricDefinition,
+                @JsonProperty("value") double value,
+                @JsonProperty("timestamp") long timestamp) {
+        }
+    }
+    
 }
