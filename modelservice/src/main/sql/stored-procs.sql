8ffccb7ef72b1189e26bb694f0012e3d05a155e3
shsethi <shsethi@expedia.com>
2018-10-11 16:43:15 +0530

    [MODELSERVICE] modifying wildcard insertion stored proc

--- modelservice/src/main/sql/stored-procs.sql
+++ modelservice/src/main/sql/stored-procs.sql
@@ -4,7 +4,6 @@ DROP PROCEDURE IF EXISTS insert_mapping;
 DROP PROCEDURE IF EXISTS insert_model;
 DROP PROCEDURE IF EXISTS insert_detector;
 
-DROP PROCEDURE IF EXISTS insert_mapping;
 DROP PROCEDURE IF EXISTS insert_mapping_wildcard_metric_targets_to_model;
 DELIMITER //
 
@@ -43,13 +42,13 @@ CREATE PROCEDURE insert_mapping (
     INSERT INTO metric_detector_mapping (metric_id, detector_id) VALUES (metric_id, detector_id);
   END //
 
-CREATE PROCEDURE insert_mapping_wildcard_metric_targets_to_model (
-  IN model_uuid CHAR(36),
+CREATE PROCEDURE insert_mapping_wildcard_metric_targets_to_detector (
+  IN detector_uuid CHAR(36),
   IN metric_ukey CHAR(100)
  )
   BEGIN
     DECLARE metric_id INT(10) UNSIGNED;
-    DECLARE model_id INT(10) UNSIGNED;
+    DECLARE detector_id INT(10) UNSIGNED;
     DECLARE done INT DEFAULT 0;
     DECLARE present INT DEFAULT 0;
     DECLARE cur1 cursor for SELECT m.id FROM metric m WHERE m.ukey LIKE metric_ukey;
@@ -60,10 +59,10 @@ CREATE PROCEDURE insert_mapping_wildcard_metric_targets_to_model (
     REPEAT
     	FETCH cur1 into metric_id;
     	if NOT done then
-    	SELECT id INTO model_id FROM model WHERE uuid = model_uuid;
-    	IF NOT EXISTS (SELECT 1 FROM metric_model_mapping m3 WHERE m3.metric_id = metric_id and m3.model_id = model_id)
+    	SELECT id INTO detector_id FROM detector WHERE uuid = detector_uuid;
+    	IF NOT EXISTS (SELECT 1 FROM metric_detector_mapping m3 WHERE m3.metric_id = metric_id and m3.detector_id = detector_id)
     	THEN
-    	INSERT INTO metric_model_mapping (metric_id, model_id) VALUES (metric_id, model_id);
+    	INSERT INTO metric_detector_mapping (metric_id, detector_id) VALUES (metric_id, detector_id);
     	END IF;
         END IF;
     UNTIL done END REPEAT;
