5935fa704c06dbd471088b2e30518cf9a43cf53b
Karan Shah <kashah@expedia.com>
2019-01-03 14:18:01 +0530

    added oauth client table

--- modelservice/src/main/sql/build-db.sql
+++ modelservice/src/main/sql/build-db.sql
@@ -59,3 +59,17 @@ create table user (
   role         varchar(100),
   enabled      boolean
 );
+
+create table oauth_client_details (
+  client_id VARCHAR(256) PRIMARY KEY,
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additional_information VARCHAR(4096),
+  autoapprove VARCHAR(256)
+);
