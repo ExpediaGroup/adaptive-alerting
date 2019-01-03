bb7eeb019baabcd3696f3a609282c5ff020b6a48
Karan Shah <kashah@expedia.com>
2019-01-03 13:54:31 +0530

    Removed unused import

--- modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/security/ResourceServerConfig.java
+++ modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/security/ResourceServerConfig.java
@@ -19,7 +19,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
 /**
  * Resource server hosting the protected resource.
