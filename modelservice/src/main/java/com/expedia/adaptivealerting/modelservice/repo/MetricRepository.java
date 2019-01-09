de3df45edffb9122843f1154c28425a373d127e4
Karan Shah <kashah@expedia.com>
2019-01-09 13:14:59 +0530

    updated comment

--- modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/repo/MetricRepository.java
+++ modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/repo/MetricRepository.java
@@ -80,7 +80,7 @@ public interface MetricRepository extends PagingAndSortingRepository<Metric, Lon
      * Finds a list of metrics attached to a given detector
      *
      * @param uuid Detector uuid.
-     * @return List of metrics attached to that detector uuid
+     * @return List of metrics attached to the provided detector uuid
      */
     @Query("select mmm.metric from MetricDetectorMapping mmm where mmm.detector.uuid = :uuid")
     List<Metric> findByDetectorUuid(@Param("uuid") String uuid);
