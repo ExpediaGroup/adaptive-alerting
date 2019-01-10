04e476b559a87284b301dc31b87230f0103db44c
Karan Shah <kashah@expedia.com>
2019-01-10 17:00:38 +0530

    removed unused libs

--- modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/security/SecurityConfig.java
+++ modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/security/SecurityConfig.java
@@ -40,8 +40,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-import javax.sql.DataSource;
-
 /**
  * @author kashah
  */
