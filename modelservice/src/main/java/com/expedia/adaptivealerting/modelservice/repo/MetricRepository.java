9d32bc7148e0d110cceda25e61dabe4ce642f17d
Tushar Bahl <tbahl@DELC02SK080G8WN.sea.corp.expecn.com>
2019-01-11 11:39:38 +0530

    Adding method for finding detector UUID

--- modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/repo/MetricRepository.java
+++ modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/repo/MetricRepository.java
@@ -77,6 +77,15 @@ public interface MetricRepository extends PagingAndSortingRepository<Metric, Lon
     @Query(nativeQuery = true, value = "SELECT m.* FROM metric m INNER JOIN metric_tag_mapping mm on mm.metric_id = m.id  INNER JOIN tag t on t.id = mm.tag_id WHERE t.ukey = :tagKey AND t.uvalue = :tagValue")
     List<Metric> findByTagContaining(@Param("tagKey") String tagKey, @Param("tagValue") String tagValue, Pageable pageable);
 
+    /**
+     * Finds a list of metrics attached to a given detector
+     *
+     * @param uuid Detector uuid.
+     * @return List of metrics attached to the provided detector uuid
+     */
+    @Query("select mmm.metric from MetricDetectorMapping mmm where mmm.detector.uuid = :uuid")
+    List<Metric> findByDetectorUuid(@Param("uuid") String uuid);
+
     @Override
     Metric save(Metric metric);
 
