aa67739314f05522e70b0daa1b26e133b0629dda
Willie Wheeler <willie.wheeler@gmail.com>
2019-03-07 22:03:10 +1000

    Added detector source unit tests

--- anomdetect/src/test/java/com/expedia/adaptivealerting/anomdetect/source/DefaultDetectorSourceTest.java
+++ anomdetect/src/test/java/com/expedia/adaptivealerting/anomdetect/source/DefaultDetectorSourceTest.java
@@ -15,9 +15,9 @@
  */
 package com.expedia.adaptivealerting.anomdetect.source;
 
-import com.expedia.adaptivealerting.anomdetect.util.DetectorResource;
-import com.expedia.adaptivealerting.anomdetect.util.ModelServiceConnector;
-import com.expedia.adaptivealerting.anomdetect.util.ModelTypeResource;
+import com.expedia.adaptivealerting.anomdetect.AnomalyDetector;
+import com.expedia.adaptivealerting.anomdetect.ewma.EwmaAnomalyDetector;
+import com.expedia.adaptivealerting.anomdetect.util.*;
 import com.expedia.metrics.MetricDefinition;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -28,50 +28,96 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.hateoas.Resources;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
 @Slf4j
 public final class DefaultDetectorSourceTest {
-    private static final String DETECTOR_UUID = "90c37a3c-f6bb-4c00-b41b-191909cccfb7";
+    private static final UUID DETECTOR_UUID = UUID.fromString("90c37a3c-f6bb-4c00-b41b-191909cccfb7");
+    private static final UUID DETECTOR_UUID_MISSING_DETECTOR = UUID.fromString("90c37a3c-f6bb-4c00-b41b-191909cccfb8");
     private static final String DETECTOR_TYPE = "ewma-detector";
     
-    // Class under test
-    private DefaultDetectorSource source;
+    private DefaultDetectorSource sourceUnderTest;
     
     @Mock
     private ModelServiceConnector connector;
-    
+
     private MetricDefinition metricDef;
+    private DetectorMeta detectorMeta;
+    private DetectorMeta detectorMetaMissingDetector;
     private Resources<DetectorResource> detectorResources;
-    
+    private ModelResource modelResource;
+    private AnomalyDetector detector;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        this.source = new DefaultDetectorSource(connector);
+        this.sourceUnderTest = new DefaultDetectorSource(connector);
         initTestObjects();
         initDependencies();
     }
-    
+
+    @Test
+    public void testFindDetectorTypes() {
+        assertNotNull(sourceUnderTest.findDetectorTypes());
+    }
+
     @Test
     public void testFindDetectorMetas() {
-        val results = source.findDetectorMetas(metricDef);
+        val results = sourceUnderTest.findDetectorMetas(metricDef);
         assertEquals(1, results.size());
         
         val result = results.get(0);
-        assertEquals(DETECTOR_UUID, result.getUuid().toString());
+        assertEquals(DETECTOR_UUID, result.getUuid());
         assertEquals(DETECTOR_TYPE, result.getType());
     }
-    
+
+    @Test
+    public void testFindDetector() {
+        val result = sourceUnderTest.findDetector(detectorMeta, metricDef);
+        assertNotNull(result);
+        assertEquals(detectorMeta.getUuid(), result.getUuid());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindDetector_nullMeta() {
+        sourceUnderTest.findDetector(null, metricDef);
+    }
+
+    @Test
+    public void testFindDetector_missingDetector() {
+        val result = sourceUnderTest.findDetector(detectorMetaMissingDetector, metricDef);
+        assertNull(result);
+    }
+
     private void initTestObjects() {
         this.metricDef = new MetricDefinition("my-metric");
-        
-        val detectorResource = new DetectorResource(DETECTOR_UUID, new ModelTypeResource(DETECTOR_TYPE));
+
+        this.detectorMeta = new DetectorMeta(DETECTOR_UUID, DETECTOR_TYPE);
+        this.detectorMetaMissingDetector = new DetectorMeta(DETECTOR_UUID_MISSING_DETECTOR, DETECTOR_TYPE);
+
+        val detectorResource = new DetectorResource(DETECTOR_UUID.toString(), new ModelTypeResource(DETECTOR_TYPE));
         this.detectorResources = new Resources<>(Collections.singletonList(detectorResource));
+
+        val params = new HashMap<String, Object>();
+        params.put("alpha", 0.2);
+        params.put("weakSigmas", 2.0);
+        params.put("strongSigmas", 4.0);
+
+        this.modelResource = new ModelResource();
+        modelResource.setUuid(DETECTOR_UUID);
+        modelResource.setParams(params);
+        modelResource.setDetectorType(new ModelTypeResource(DETECTOR_TYPE));
+
+        this.detector = new EwmaAnomalyDetector();
     }
     
     private void initDependencies() {
         when(connector.findDetectors(metricDef)).thenReturn(detectorResources);
+        when(connector.findLatestModel(DETECTOR_UUID)).thenReturn(modelResource);
+        when(connector.findLatestModel(DETECTOR_UUID_MISSING_DETECTOR)).thenReturn(null);
     }
 }
