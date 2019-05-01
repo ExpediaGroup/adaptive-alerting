c56e515acb53de39ea15d39aa2fa8e0dbb2700bb
dinilatgit <ddivakar@expedia.com>
2019-05-01 13:04:19 +1000

    Add test cases for ElasticSearchMappingDetectorMappingService

--- modelservice/src/test/java/com/expedia/adaptivealerting/modelservice/dao/es/ElasticSearchDetectorMappingServiceTest.java
+++ modelservice/src/test/java/com/expedia/adaptivealerting/modelservice/dao/es/ElasticSearchDetectorMappingServiceTest.java
@@ -19,10 +19,14 @@ import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import com.expedia.adaptivealerting.modelservice.model.Detector;
+import com.expedia.adaptivealerting.modelservice.model.DetectorMapping;
 import com.expedia.adaptivealerting.modelservice.model.MatchingDetectorsResponse;
+import com.expedia.adaptivealerting.modelservice.model.SearchMappingsRequest;
 import lombok.val;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
@@ -47,6 +51,7 @@ import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
@@ -72,17 +77,13 @@ public class ElasticSearchDetectorMappingServiceTest {
         when(elasticSearchConfig.getDocType()).thenReturn("details");
         when(elasticSearchConfig.getConnectionTimeout()).thenReturn(100);
         detectorMappingService = new ElasticSearchDetectorMappingService(metricRegistry);
-        ReflectionTestUtils.setField(detectorMappingService,"elasticSearchClient", elasticSearchClient);
-        ReflectionTestUtils.setField(detectorMappingService,"elasticSearchConfig", elasticSearchConfig);
+        ReflectionTestUtils.setField(detectorMappingService, "elasticSearchClient", elasticSearchClient);
+        ReflectionTestUtils.setField(detectorMappingService, "elasticSearchConfig", elasticSearchConfig);
     }
 
     @Test
     public void findMatchingDetectorMappings_successful() throws IOException {
         List<Map<String, String>> tagsList = new ArrayList<>();
-        Map<String, String> tags = new HashMap<>();
-        tags.put("name", "sample-app");
-        tags.put("env", "prod");
-        tags.put("type", "gauge");
         val searchIndex = "2";
         val lookUpTime = 100;
         val detectorUuid = "aeb4d849-847a-45c0-8312-dc0fcf22b639";
@@ -101,17 +102,122 @@ public class ElasticSearchDetectorMappingServiceTest {
     @Test(expected = RuntimeException.class)
     public void findMatchingDetectorMappings_fail() throws IOException {
         List<Map<String, String>> tagsList = new ArrayList<>();
-        Map<String, String> tags = new HashMap<>();
-        tags.put("name", "sample-app");
-        tags.put("env", "prod");
-        tags.put("type", "gauge");
         when(elasticSearchClient.search(any(SearchRequest.class), eq(RequestOptions.DEFAULT))).thenThrow(new IOException());
         detectorMappingService.findMatchingDetectorMappings(tagsList);
     }
 
-    private SearchResponse mockSearchResponse(String searchIndex, int lookUpTime, String detectorUuid) {
+    @Test
+    public void findDetectorMapping_successful() throws IOException {
+        List<Map<String, String>> tagsList = new ArrayList<>();
+        val detectorUuid = "aeb4d849-847a-45c0-8312-dc0fcf22b639";
+        String id = "adsvade8^szx";
+        Long LastModifiedTimeInMillis = new Long (1554828886);
+        Long CreatedTimeInMillis = new Long(1554828886);
+        GetResponse getResponse = mockGetResponse(id);
+        when(elasticSearchClient.get(any(GetRequest.class), eq(RequestOptions.DEFAULT))).thenReturn(getResponse);
+        DetectorMapping detectorMapping = detectorMappingService.findDetectorMapping(id);
+        verify(elasticSearchClient, atLeastOnce()).get(any(GetRequest.class), eq(RequestOptions.DEFAULT));
+        assertNotNull("Response can't be null", detectorMapping);
+        assertEquals(id, detectorMapping.getId());
+        assertEquals("test-user", detectorMapping.getUser().getId().toString());
+        assertEquals(LastModifiedTimeInMillis, Long.valueOf(detectorMapping.getLastModifiedTimeInMillis()));
+        assertEquals(CreatedTimeInMillis, Long.valueOf(detectorMapping.getCreatedTimeInMillis()));
+        assertTrue(detectorMapping.isEnabled());
+        assertEquals(UUID.fromString(detectorUuid), detectorMapping.getDetector().getId());
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void findDetectorMapping_fail() throws IOException {
+        String id = "adsvade8^szx";
+        when(elasticSearchClient.get(any(GetRequest.class), eq(RequestOptions.DEFAULT))).thenThrow(new IOException());
+        detectorMappingService.findDetectorMapping(id);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void findMatchingDetectorMappings_tagsListforloopcheck() throws IOException {
+        List<Map<String, String>> tagsList = new ArrayList<>();
+        Map<String, String> tags = new HashMap<>();
+        tags.put("name", "sample-app");
+        tags.put("env", "prod");
+        tags.put("type", "gauge");
+        tagsList.add(tags);
+        when(elasticSearchClient.search(any(SearchRequest.class), eq(RequestOptions.DEFAULT))).thenThrow(new IOException());
+        detectorMappingService.findMatchingDetectorMappings(tagsList);
+    }
+
+    @Test
+    public void findLastUpdated_successful() throws IOException {
+        List<DetectorMapping> tagsList = new ArrayList<>();
+        Map<String, String> tags = new HashMap<>();
+        Long LastModifiedTimeInMillis = new Long (1554828886);
+        Long CreatedTimeInMillis = new Long(1554828886);
+        val searchIndex = "2";
+        val lookUpTime = 100;
+        val detectorUuid = "aeb4d849-847a-45c0-8312-dc0fcf22b639";
+        int TimeinSeconds = 60;
+        SearchResponse searchResponse = mockSearchResponse(searchIndex, lookUpTime, detectorUuid);
+        when(elasticSearchClient.search(any(SearchRequest.class), eq(RequestOptions.DEFAULT))).thenReturn(searchResponse);
+        tagsList = detectorMappingService.findLastUpdated(TimeinSeconds);
+        verify(elasticSearchClient, atLeastOnce()).search(any(SearchRequest.class), eq(RequestOptions.DEFAULT));
+        assertNotNull("Response can't be null", tagsList);
+        assertEquals(1, tagsList.size());
+        assertEquals(UUID.fromString(detectorUuid), tagsList.get(0).getDetector().getId());
+        assertEquals("test-user", tagsList.get(0).getUser().getId().toString());
+        assertEquals(LastModifiedTimeInMillis, Long.valueOf(tagsList.get(0).getLastModifiedTimeInMillis()));
+        assertEquals(CreatedTimeInMillis, Long.valueOf(tagsList.get(0).getCreatedTimeInMillis()));
+        assertTrue(tagsList.get(0).isEnabled());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void findLastUpdated_fail() throws IOException {
+        int TimeinSeconds = 60;
+        when(elasticSearchClient.search(any(SearchRequest.class), eq(RequestOptions.DEFAULT))).thenThrow(new IOException());
+        detectorMappingService.findLastUpdated(TimeinSeconds);
+    }
+
+    @Test
+    public void search_successful() throws IOException {
+        List<DetectorMapping> tagsList = new ArrayList<>();
+        Map<String, String> tags = new HashMap<>();
+        Long LastModifiedTimeInMillis = new Long (1554828886);
+        Long CreatedTimeInMillis = new Long(1554828886);
+        val searchIndex = "2";
+        val lookUpTime = 100;
+        val detectorUuid = "aeb4d849-847a-45c0-8312-dc0fcf22b639";
+        SearchMappingsRequest searchMappingsRequest = new SearchMappingsRequest();
+        SearchResponse searchResponse = mockSearchResponse(searchIndex, lookUpTime, detectorUuid);
+        when(elasticSearchClient.search(any(SearchRequest.class), eq(RequestOptions.DEFAULT))).thenReturn(searchResponse);
+        tagsList = detectorMappingService.search(searchMappingsRequest);
+        verify(elasticSearchClient, atLeastOnce()).search(any(SearchRequest.class), eq(RequestOptions.DEFAULT));
+        assertNotNull("Response can't be null", tagsList);
+        assertEquals(1, tagsList.size());
+        assertEquals(UUID.fromString(detectorUuid), tagsList.get(0).getDetector().getId());
+        assertEquals("test-user", tagsList.get(0).getUser().getId().toString());
+        assertEquals(LastModifiedTimeInMillis, Long.valueOf(tagsList.get(0).getLastModifiedTimeInMillis()));
+        assertEquals(CreatedTimeInMillis, Long.valueOf(tagsList.get(0).getCreatedTimeInMillis()));
+        assertTrue(tagsList.get(0).isEnabled());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void search_conditionalbranches() throws IOException {
+        SearchMappingsRequest searchMappingsRequest = new SearchMappingsRequest();
+        searchMappingsRequest.setUserId("test-user");
+        searchMappingsRequest.setDetectorUuid(UUID.fromString("aeb4d849-847a-45c0-8312-dc0fcf22b639"));
+        when(elasticSearchClient.search(any(SearchRequest.class), eq(RequestOptions.DEFAULT))).thenThrow(new IOException());
+        detectorMappingService.search(searchMappingsRequest);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void search_fail() throws IOException {
+        int TimeinSeconds = 60;
+        when(elasticSearchClient.search(any(SearchRequest.class), eq(RequestOptions.DEFAULT))).thenThrow(new IOException());
+        detectorMappingService.findLastUpdated(TimeinSeconds);
+    }
+
+    private SearchResponse mockSearchResponse(String searchIndex,int lookUpTime, String detectorUuid){
         SearchResponse searchResponse = mock(SearchResponse.class);
-        Map<String, DocumentField> fields =  new HashMap<>();
+        Map<String, DocumentField> fields = new HashMap<>();
         fields.put("_percolator_document_slot", new DocumentField("_percolator_document_slot", Arrays.asList(new Integer(searchIndex))));
         SearchHit searchHit = new SearchHit(101, "xxx", null, fields);
         BytesReference source = new BytesArray("{\"aa_user\":{\"id\":\"test-user\"},\"aa_detector\":" +
@@ -126,5 +232,18 @@ public class ElasticSearchDetectorMappingServiceTest {
         TimeValue timeValue = new TimeValue(lookUpTime);
         when(searchResponse.getTook()).thenReturn(timeValue);
         return searchResponse;
+    }
+
+    private GetResponse mockGetResponse(String id) {
+        GetResponse getResponse = mock(GetResponse.class);
+        Map<String, DocumentField> fields = new HashMap<>();
+        val detectorUuid = "aeb4d849-847a-45c0-8312-dc0fcf22b639";
+        String source = new String("{\"aa_user\":{\"id\":\"test-user\"},\"aa_detector\":" +
+                "{\"id\":\"" + detectorUuid + "\"},\"aa_query\":{\"bool\":{\"must\":[{\"match\":" +
+                "{\"name\":\"sample-web\",\"env\":\"prod\"}}]}},\"aa_enabled\":true,\"aa_lastModifiedTime\":1554828886," +
+                "\"aa_createdTime\":1554828886}\n");
+        when(getResponse.getSourceAsString()).thenReturn(source);
+        when(getResponse.getId()).thenReturn(id);
+        return getResponse;
     }
 }
