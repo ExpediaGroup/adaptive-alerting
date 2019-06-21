562c085009056c3bd440f4ba8ecb7c251f6dfb73
Willie Wheeler <willie.wheeler@gmail.com>
2019-06-21 23:43:33 +1000

    Unit test improvements

--- anomdetect/src/test/java/com/expedia/adaptivealerting/anomdetect/breakout/EdmxTest.java
+++ anomdetect/src/test/java/com/expedia/adaptivealerting/anomdetect/breakout/EdmxTest.java
@@ -19,6 +19,7 @@ import com.expedia.adaptivealerting.anomdetect.util.MetricFrameLoader;
 import com.expedia.adaptivealerting.anomdetect.util.TestObjectMother;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -30,6 +31,15 @@ import static org.junit.Assert.assertEquals;
 @Slf4j
 public class EdmxTest {
     private static final double TOLERANCE = 0.001;
+    private static final long RANDOM_SEED = 314159;
+
+    private Random random;
+
+    @Before
+    public void setUp() {
+        this.random = new Random();
+        random.setSeed(RANDOM_SEED);
+    }
 
     @Test
     public void testEdmx_range0() {
@@ -45,35 +55,26 @@ public class EdmxTest {
     }
 
     @Test
-    public void testEdmx_noBreakout_noPerms() {
+    public void testEdmx_noBreakout() {
         val data = new ArrayList<Double>(100);
         for (int i = 0; i < 50; i++) {
             data.add(1.0);
             data.add(1.2);
         }
-        val result = Edmx.edmx(data, 5, 0);
 
         // When there's no permutation test, the algo returns the location with the highest statistic.
         // Or one of them, I guess, when there are multiple such locations (haven't checked).
+        val result = Edmx.edmx(data, 5, 0);
         assertEquals(49, result.getLocation());
         assertEquals(24.99, result.getStat(), TOLERANCE);
+
+        // With permutations
+        val result2 = Edmx.edmx(data, 5, 20);
+        assertEquals(-1, result2.getLocation());
     }
 
     @Test
-    public void testEdmx_noBreakout_withPerms() {
-        val data = new ArrayList<Double>(100);
-        for (int i = 0; i < 50; i++) {
-            data.add(1.0);
-            data.add(1.2);
-        }
-        val result = Edmx.edmx(data, 5, 20);
-
-        // TODO Implement permutation test so we can avoid reporting a breakout where none exists.
-        assertEquals(-1, result.getLocation());
-    }
-
-    @Test
-    public void testEdmx_whiteNoiseWithBreakout1_noPerms() throws Exception {
+    public void testEdmx_whiteNoise_breakout() throws Exception {
         val metricDef = TestObjectMother.metricDefinition();
         val is = ClassLoader.getSystemResourceAsStream("datasets/white-noise-with-breakout-at-row-600.csv");
         val metricFrame = MetricFrameLoader.loadCsv(metricDef, is, false);
@@ -82,35 +83,19 @@ public class EdmxTest {
                 .map(metricData -> metricData.getValue())
                 .collect(Collectors.toList());
 
+        // No permutations
         val result = Edmx.edmx(data, 24, 0);
-        log.trace("result={}", result);
-
-        // These are the values that Twitter's BreakoutDetection R package produces.
         assertEquals(600, result.getLocation());
         assertEquals(18.02054, result.getStat(), TOLERANCE);
+
+        // With permutations
+        val result2 = Edmx.edmx(data, 24, 20);
+        assertEquals(600, result2.getLocation());
+        assertEquals(18.02054, result2.getStat(), TOLERANCE);
     }
 
     @Test
-    public void testEdmx_whiteNoiseWithBreakout1_withPerms() throws Exception {
-        val metricDef = TestObjectMother.metricDefinition();
-        val is = ClassLoader.getSystemResourceAsStream("datasets/white-noise-with-breakout-at-row-600.csv");
-        val metricFrame = MetricFrameLoader.loadCsv(metricDef, is, false);
-        val metricDataList = metricFrame.getMetricData();
-        val data = metricDataList.stream()
-                .map(metricData -> metricData.getValue())
-                .collect(Collectors.toList());
-
-        val result = Edmx.edmx(data, 24, 20);
-        log.trace("result={}", result);
-
-        // These are the values that Twitter's BreakoutDetection R package produces.
-        assertEquals(600, result.getLocation());
-        assertEquals(18.02054, result.getStat(), TOLERANCE);
-    }
-
-    @Test
-    public void testEdmx() {
-        val random = new Random();
+    public void testEdmx_longSeries_smallBreakout() {
         val data = new ArrayList<Double>(100);
         for (int i = 0; i < 80; i++) {
             data.add(random.nextGaussian());
@@ -119,13 +104,14 @@ public class EdmxTest {
             data.add(random.nextGaussian() + 3.0);
         }
 
+        // No permutations, small breakout
         val result = Edmx.edmx(data, 10, 0);
-        log.info("result={}", result);
+        assertEquals(75, result.getLocation());
+        assertEquals(2.569, result.getStat(), TOLERANCE);
     }
 
     @Test
-    public void testEdmx2() {
-        val random = new Random();
+    public void testEdmx_shortSeries_smallBreakout() {
         val data = new ArrayList<Double>(10);
         for (int i = 0; i < 8; i++) {
             data.add(random.nextGaussian());
@@ -134,13 +120,14 @@ public class EdmxTest {
             data.add(random.nextGaussian() + 3.0);
         }
 
+        // No permutations, small breakout
         val result = Edmx.edmx(data, 2, 0);
-        log.info("result={}", result);
+        assertEquals(8, result.getLocation());
+        assertEquals(0.455, result.getStat(), TOLERANCE);
     }
 
     @Test
-    public void testEdmx3() {
-        val random = new Random();
+    public void testEdmx_shortSeries_largeBreakout() {
         val data = new ArrayList<Double>(10);
         for (int i = 0; i < 8; i++) {
             data.add(random.nextGaussian());
@@ -149,7 +136,9 @@ public class EdmxTest {
             data.add(random.nextGaussian() + 100.0);
         }
 
+        // No permutations, large breakout
         val result = Edmx.edmx(data, 2, 0);
-        log.info("result={}", result);
+        assertEquals(7, result.getLocation());
+        assertEquals(1.986, result.getStat(), TOLERANCE);
     }
 }
