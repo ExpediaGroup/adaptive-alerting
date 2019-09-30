cdf816e7330b1b68126939b5c804fb14c476e64f
dinilatgit <ddivakar@expedia.com>
2019-09-30 20:43:31 +1000

    add deployment templates for metric functions

---
+++ kafka/docker/scripts/build-aa-metric-fucntions.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+ # Copyright 2019 Expedia Group, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+APP_NAME="adaptive-alerting-metrics-functions"
+DOTTED_APP_NAME="metric.functions"
+MAIN_CLASS="com.expedia.adaptivealerting.kafka.KafkaMetricFunctions"
+
+cd `cd -P -- "$(dirname -- "$0")" && pwd -P`
+
+. common.sh
