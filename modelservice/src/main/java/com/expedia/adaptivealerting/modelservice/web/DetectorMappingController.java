035c65dac7c0218624af492cb42334c0ae108376
Karan Shah <kashah@expedia.com>
2019-12-03 11:49:18 +0530

    minor formatting change

--- modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/web/DetectorMappingController.java
+++ modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/web/DetectorMappingController.java
@@ -75,7 +75,7 @@ public class DetectorMappingController {
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     public List<DetectorMapping> searchDetectorMapping(@RequestBody SearchMappingsRequest request) {
         AssertUtil.isTrue(request.getUserId() != null || request.getDetectorUuid() != null,
-                "user id and detector UUID can't both be null");
+                "User id and Detector UUID can't both be null");
         return detectorMappingRepo.search(request);
     }
 
