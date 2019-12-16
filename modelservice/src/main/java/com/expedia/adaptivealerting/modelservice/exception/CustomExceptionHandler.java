ca72e716cfd051e50e02ed11e38618f89e7d5ae3
Tushar Bahl <tbahl@expedia.com>
2019-12-16 20:51:38 +0530

    adding logging for the exception in modelservice.

--- modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/exception/CustomExceptionHandler.java
+++ modelservice/src/main/java/com/expedia/adaptivealerting/modelservice/exception/CustomExceptionHandler.java
@@ -18,6 +18,7 @@ package com.expedia.adaptivealerting.modelservice.exception;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.Generated;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -31,6 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings({"unchecked", "rawtypes"})
+@Slf4j
 @ControllerAdvice
 @Generated
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -47,19 +49,18 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         List<String> details = new ArrayList<>();
         details.add(ex.getLocalizedMessage());
         ErrorResponse error = new ErrorResponse("Server Error", details);
+        log.error("Exception occurred "+ error);
         exceptionCounter.increment();
         return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = {RecordNotFoundException.class})
     protected ResponseEntity<Object> handleRecordNotFound(final RuntimeException ex, final WebRequest request) {
-        exceptionCounter.increment();
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
     public ResponseEntity<Object> handleIllegalArgument(final RuntimeException ex, final WebRequest request) {
-        exceptionCounter.increment();
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
