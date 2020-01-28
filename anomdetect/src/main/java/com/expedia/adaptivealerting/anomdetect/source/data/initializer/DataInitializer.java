3118aaab94549552497413a2bf616d24c0373401
Karan Shah <kashah@expedia.com>
2020-01-28 12:45:54 +0530

    Added logging for data initializer

--- anomdetect/src/main/java/com/expedia/adaptivealerting/anomdetect/source/data/initializer/DataInitializer.java
+++ anomdetect/src/main/java/com/expedia/adaptivealerting/anomdetect/source/data/initializer/DataInitializer.java
@@ -55,10 +55,13 @@ public class DataInitializer {
 
     public void initializeDetector(MappedMetricData mappedMetricData, Detector detector) {
         if (detector != null && isSeasonalNaiveDetector(detector)) {
-            val data = getHistoricalData(mappedMetricData);
-            val forecastingDetector = (ForecastingDetector) detector;
             val metricDefinition = mappedMetricData.getMetricData().getMetricDefinition();
+            log.debug("Initializing seasonal naive detector for metric " + metricDefinition);
+            val data = getHistoricalData(mappedMetricData);
+            log.debug("Fetched historical data "  + data + " for metric " + metricDefinition);
+            val forecastingDetector = (ForecastingDetector) detector;
             populateForecastingDetectorWithHistoricalData(forecastingDetector, data, metricDefinition);
+            log.debug("Populated detector with historical data for metric " + metricDefinition);
         }
     }
 
