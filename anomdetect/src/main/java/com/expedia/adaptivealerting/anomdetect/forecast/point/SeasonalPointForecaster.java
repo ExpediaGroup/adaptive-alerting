69a529cca2d9ee62bb55165811ed5ef50e46e2b4
Karan Shah <kashah@expedia.com>
2020-02-05 14:53:17 +0530

    fixed style check error

--- anomdetect/src/main/java/com/expedia/adaptivealerting/anomdetect/forecast/point/SeasonalPointForecaster.java
+++ anomdetect/src/main/java/com/expedia/adaptivealerting/anomdetect/forecast/point/SeasonalPointForecaster.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.expedia.adaptivealerting.anomdetect.forecast.point;
 
 public interface SeasonalPointForecaster extends PointForecaster {
